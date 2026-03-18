8fcce9078c7b9d4cfbf531223d00c1777298add3
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2026-03-18 19:29:35 +0100

    Fix ft_strcmp function to handle null pointers and improve comparison logic

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -12,18 +12,18 @@
 
 # include "libft.h"
 
-int		ft_strcmp(const char *s1, const char *s2)
+int ft_strcmp(const char *s1, const char *s2)
 {
-    if ((!s2))
-		return (-1);
-	if ((!s1))
-		return (1);
-    int i = 0;
-    while (s1[i] || s2[i])
-    {
-        if (s1[i] != s2[i])
+	int i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-    }
-    return (0);
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
