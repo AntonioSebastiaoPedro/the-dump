939711837423bc570335d90591370cc7f9d766d0
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2026-03-19 10:24:30 +0100

    Refactor parse_texture function to handle texture paths for SO, WE, and EA; improve error handling and memory management

--- parser/parser_testura.c
+++ parser/parser_testura.c
@@ -14,6 +14,8 @@
 
 int parse_texture(const char *line, t_game *game)
 {
+	char	*tmp;
+	int		fd_testura;
     char    **parts;
 
    parts = ft_split(line, ESPACO);
@@ -24,24 +26,56 @@ int parse_texture(const char *line, t_game *game)
 	{
 		if (game->no)
 			return (ft_putendl_fd("Erro: NO já definido", 2), 0);
+		tmp = ft_strtrim(parts[1], " \n\r\t");
+		free(parts[1]);
+		parts[1] = tmp;
+		fd_testura = open(parts[1], O_RDONLY);
+		printf("Caminho [%s]\n", parts[1]);
+		if (fd_testura < 0)
+			return (ft_putendl_fd("Erro: Caminho da Textura NO invalida", 2), 0);
 		game->no = ft_strdup(parts[1]);
+		close(fd_testura);
 	}
 	else if (ft_strcmp(parts[0], "SO") == 0)
 	{
 		if (game->so)
 			return (ft_putendl_fd("Erro: SO já definido", 2), 0);
+		tmp = ft_strtrim(parts[1], " \n\r\t");
+		free(parts[1]);
+		parts[1] = tmp;
+		fd_testura = open(parts[1], O_RDONLY);
+		printf("Caminho [%s]\n", parts[1]);
+		if (fd_testura < 0)
+			return (ft_putendl_fd("Erro: Caminho da Textura SO invalida", 2), 0);
 		game->so = ft_strdup(parts[1]);
+		close(fd_testura);
 	}
 	else if (ft_strcmp(parts[0], "WE") == 0)
 	{
 		if (game->we)
 			return (ft_putendl_fd("Erro: WE já definido", 2), 0);
+		tmp = ft_strtrim(parts[1], " \n\r\t");
+		free(parts[1]);
+		parts[1] = tmp;
+		fd_testura = open(parts[1], O_RDONLY);
+		printf("Caminho [%s]\n", parts[1]);
+		if (fd_testura < 0)
+			return (ft_putendl_fd("Erro: Caminho da Textura WE invalida", 2), 0);
+		close(fd_testura);
 		game->we = ft_strdup(parts[1]);
 	}
 	else if (ft_strcmp(parts[0], "EA") == 0)
 	{
 		if (game->ea)
 			return (ft_putendl_fd("Erro: EA já definido", 2), 0);
+		tmp = ft_strtrim(parts[1], " \n\r\t");
+		free(parts[1]);
+		parts[1] = tmp;
+		fd_testura = open(parts[1], O_RDONLY);
+		printf("Caminho [%s]\n", parts[1]);
+		if (fd_testura < 0)
+			return (ft_putendl_fd("Erro: Caminho da Textura EA invalida", 2), 0);
+		close(fd_testura);
 		game->ea = ft_strdup(parts[1]);
 	}
 	else
